7d84c79d19f531e5db014a56583f470fdc1976f0
Daniel Calvin <arockne2003@yahoo.com>
2021-11-03 11:12:52 -0700

    Add Navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-
-function Header() {
-  return (
-    <header>Fantasylist</header>
-  )
-}
-
 function App() {
+  
+  function Header() {
+    return (
+      <header>Fantasylist</header>
+    )
+  }
+
+  function Navbar() {
+    return (
+      <nav>
+        <ul>
+          <li>Shop1</li>
+          <li>Shop2</li>
+          <li>Shop3</li>
+        </ul>
+      </nav>
+    )
+  }
   return (
     <div className="App">
       <Header />
+      <Navbar />
+      
     </div>
   );
 }
