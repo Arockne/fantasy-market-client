f041d2c1b911c6a1ecdb5e82b68d1f3ef27d20d2
Daniel Calvin <arockne2003@yahoo.com>
2021-11-03 17:04:54 -0700

    Fetch shop data with items and setShops to data from fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from './Header'
 import Navbar from './Navbar'
 import Search from './Search'
 import Shops from './Shops'
 
 function App() {
+  const [shops, setShops] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/shops/items')
+    .then(resp => resp.json())
+    .then(data => setShops(data))
+  },[])
+
+  console.log(shops)
 
   return (
     <div className="App">
       <Header />
       <Navbar />
       <Search />
-      <Shops />
+      <Shops shops={shops}/>
     </div>
   );
 }
