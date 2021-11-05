7f8e061b611fe41ca70d12537c6604f6e9aaf412
Daniel Calvin <arockne2003@yahoo.com>
2021-11-05 12:57:25 -0700

    Add two buttons

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,12 +4,17 @@ import { creationDate } from '../helpers'
 function Item({ item }) {
   const { name, desc, pounds, cost, created_at, category } = item
   const {month, monthDay} = creationDate(created_at)
+
   return (
     <div>
       <h3>{`${month} ${monthDay}`} {name} <span>{category}</span></h3>
       <p>{`${cost} gp`}</p>
       <p>{`${pounds} lbs`}</p>
       <p>{desc}</p>
+      <div>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   )
 }
