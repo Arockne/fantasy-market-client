1cd5ac3a228adcb28cbf3a0b9e5ef37943e41740
Daniel Calvin <arockne2003@yahoo.com>
2021-11-05 16:09:22 -0700

    Implement handleFormChange

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -9,6 +9,19 @@ function ItemForm({ shops }) {
     desc: ''
   })
 
+  function handleFormChange(e) {
+    const {name, value} = e.target
+    
+    const updatedForm = {...form, [name]: value}
+    
+    if (name === 'shopID') {
+      const shop = shops.find(({id}) => id === parseInt(value, 10))
+      updatedForm.category = shop.specialization
+    }
+
+    setForm(updatedForm)
+  }
+  
   return (
     <form>
       <label htmlFor='name'>Name: </label>
@@ -16,23 +29,33 @@ function ItemForm({ shops }) {
         id='name'
         name='name'
         type='text'
+        onChange={handleFormChange}
       />
       <label htmlFor='shop'>Shop: </label>
-      <select id='shop' name='shopID'>
+      <select 
+        id='shop' 
+        name='shopID'
+        onChange={handleFormChange}
+      >
         {shops.map(shop => {
          return <option key={shop.id} value={shop.id}>{shop.name} - {shop.specialization}</option> 
         })}
       </select>
 
-      <label htmlFor='cost'>Cost: </label>
+      <label htmlFor='cost'>Cost in Gold: </label>
       <input
         id='cost' 
         type='number' 
         name='cost'
+        onChange={handleFormChange}
       />
 
       <label htmlFor='desc'>Item Description: </label>
-      <textarea id='desc' name='desc'/>
+      <textarea 
+        id='desc' 
+        name='desc'
+        onChange={handleFormChange}
+      />
     </form>
   )
 }
