4c32bbc8f76d668d779e610065dc4e154099378b
Daniel Calvin <arockne2003@yahoo.com>
2021-11-05 08:46:58 -0700

    Create helpers.js and implement creationDate

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { creationDate } from '../helpers'
 
-function Item() {
+function Item({ item }) {
+  const { name, desc, pounds, cost, created_at, category } = item
+  const {month, monthDay} = creationDate(created_at)
   return (
     <div>
-      {/**/}
+      <h3>{`${month} ${monthDay}`} {name} <span>{category}</span></h3>
+      <p>{`${cost} gp`}</p>
+      <p>{`${pounds} lbs`}</p>
+      <p>{desc}</p>
     </div>
   )
 }
 
 export default Item
+
+//['Wed', 'Nov', '03', '2021', '08:52:45', 'GMT-0700', '(Mountain', 'Standard', 'Time)']
