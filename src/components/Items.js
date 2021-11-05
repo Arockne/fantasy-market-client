7625479192bffed4e091e0e4d777e537fd075986
Daniel Calvin <arockne2003@yahoo.com>
2021-11-04 17:51:46 -0700

    Fetch specific shop data with items from dynamic routing

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Item from './Item'
 import { useParams } from 'react-router-dom'
 
-function Items({ shops }) {
-  const {name, id} = useParams()
-  const currentShop = shops.find( shop => shop.id === Number(id))
+function Items() {
+  const [shop, setShop] = useState({})
+  const {id} = useParams()
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/shop/${id}`)
+    .then(resp => resp.json())
+    .then(data => setShop(data))
+  }, [id])
+
+  if (!Object.keys(shop).length) return 'Loading...'
+
   return (
     <div>
-      {/*Multiple Item component*/}
-      <Item />
+      <h2>{shop.name}</h2>
+      {shop.items.map(item => <Item key={item.id} item={item}/>)}
     </div>
   )
 }
