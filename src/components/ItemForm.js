973952f0b54cb82c5555ea00cb5725318b3fb095
Daniel Calvin <arockne2003@yahoo.com>
2021-11-05 16:13:05 -0700

    Complete controlled form by placing value props into form elements

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -11,14 +11,11 @@ function ItemForm({ shops }) {
 
   function handleFormChange(e) {
     const {name, value} = e.target
-    
     const updatedForm = {...form, [name]: value}
-    
     if (name === 'shopID') {
       const shop = shops.find(({id}) => id === parseInt(value, 10))
       updatedForm.category = shop.specialization
     }
-
     setForm(updatedForm)
   }
   
@@ -30,12 +27,14 @@ function ItemForm({ shops }) {
         name='name'
         type='text'
         onChange={handleFormChange}
+        value={form.name}
       />
       <label htmlFor='shop'>Shop: </label>
       <select 
         id='shop' 
         name='shopID'
         onChange={handleFormChange}
+        value={form.shopID}
       >
         {shops.map(shop => {
          return <option key={shop.id} value={shop.id}>{shop.name} - {shop.specialization}</option> 
@@ -48,6 +47,7 @@ function ItemForm({ shops }) {
         type='number' 
         name='cost'
         onChange={handleFormChange}
+        value={form.cost}
       />
 
       <label htmlFor='desc'>Item Description: </label>
@@ -55,6 +55,7 @@ function ItemForm({ shops }) {
         id='desc' 
         name='desc'
         onChange={handleFormChange}
+        value={form.desc}
       />
     </form>
   )
