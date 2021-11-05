47fe44bbd37023e00ca579cc4ce555eea32b9583
Daniel Calvin <arockne2003@yahoo.com>
2021-11-05 12:51:42 -0700

    Pass down shops array to Shops component

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ function App() {
         >
           <Route 
             path='shops'
-            element={<Shops />} 
+            element={<Shops shops={shops}/>} 
           > 
             <Route 
               index
