841b54002b9e5f040caf0d717c610a0afa3de591
Daniel Calvin <arockne2003@yahoo.com>
2021-11-06 16:58:47 -0700

    Add Header and Navbar components

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Navbar from './Navbar'
+
+function Main() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  )
+}
+
+export default Main
