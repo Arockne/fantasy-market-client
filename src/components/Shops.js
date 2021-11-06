f5b40c9cd3b43947b827fb3069ae5d19d9cfbe60
Daniel Calvin <arockne2003@yahoo.com>
2021-11-06 16:53:54 -0700

    Remove paragraph element

--- src/components/Shops.js
+++ src/components/Shops.js
@@ -5,7 +5,6 @@ import { Outlet } from 'react-router-dom'
 function Shops({ shops }) {
   return (
     <div>
-      <p>Click on a shop to go to</p>
       <nav>
         {shops.map(shop => <Shop key={shop.id} shop={shop}/>)}
       </nav>
